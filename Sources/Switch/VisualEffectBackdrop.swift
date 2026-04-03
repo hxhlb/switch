6033c50744e78b7672dc9f20dc2181537180a089
Sanyam G <sanyamg2006@gmail.com>
2026-04-02 23:51:18 -0500

    translucent backdrop via NSVisualEffectView

---
+++ Sources/Switch/VisualEffectBackdrop.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import AppKit
+
+struct VisualEffectBackdrop: NSViewRepresentable {
+    var material: NSVisualEffectView.Material = .hudWindow
+    var blendingMode: NSVisualEffectView.BlendingMode = .behindWindow
+
+    func makeNSView(context: Context) -> NSVisualEffectView {
+        let v = NSVisualEffectView()
+        v.material = material
+        v.blendingMode = blendingMode
+        v.state = .active
+        return v
+    }
+
+    func updateNSView(_ v: NSVisualEffectView, context: Context) {
+        v.material = material
+        v.blendingMode = blendingMode
+    }
+}
